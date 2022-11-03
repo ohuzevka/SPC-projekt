38359a8d8bebedc77ff2260d97c096012b479a00
ohuzevka <95550595+ohuzevka@users.noreply.github.com>
2022-11-03 21:14:10 +0100

    DrawBorder() for serial terminal

--- projekt/Game.cpp
+++ projekt/Game.cpp
@@ -20,5 +20,25 @@ void Game::DrawBorder(const char character)
 	windows.SetPos(0, playingField.Y + 1);
 	for (int x = 0; x < playingField.X + 2; x++)	// bottom
 		cout << character;
-#endif
+#endif // WINDOWS_TERMINAL
+
+#ifdef SERIAL_TERMINAL
+	serial.SetPos(0, 0);
+	for (int x = 0; x < playingField.X + 2; x++)	// top
+		serial.Print(character, BLACK, WHITE);		// !! starta funkce, potreba opravit
+	for (int y = 0; y < playingField.Y; y++)		// left
+	{
+		serial.Print('\n', BLACK, WHITE);
+		serial.Print(character, BLACK, WHITE);
+	}
+	for (int y = 0; y < playingField.Y; y++)		// right
+	{
+		serial.SetPos(playingField.X + 1, y + 1);
+		serial.Print(character, BLACK, WHITE);
+	}
+	serial.SetPos(0, playingField.Y + 1);
+	for (int x = 0; x < playingField.X + 2; x++)	// bottom
+		serial.Print(character, BLACK, WHITE);
+
+#endif // SERIAL_TERMINAL
 }
